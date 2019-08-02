4e27e1ff77bbc962bf476557530f71dcde56e1ac
LordGhostX <solomonghost9@gmail.com>
2019-08-02 00:08:39 +0000

    added research gate scraper

---
+++ scrapers/research_gate.py
@@ -0,0 +1,24 @@
+from requests import get
+from bs4 import BeautifulSoup as Soup
+
+def research_gate():
+    profiles = ["https://www.researchgate.net/profile/Ike_Mowete", "https://www.researchgate.net/profile/Hisham_Muhammed", "https://www.researchgate.net/profile/Aa_Ayorinde"]
+    research = []
+    for profile in profiles:
+        r = get(profile).text
+        try:
+            soup = Soup(r, features="html5lib")
+        except:
+            soup = Soup(r)
+        research_section = soup.find("div", {"id": "research-items"}).find("div", {"class": "nova-o-stack"})
+        author = soup.find("div", {"class": "nova-o-pack--vertical-align-middle"}).find("div", {"class": "nova-e-text--spacing-xxs"}).text
+        for research_item in research_section.find_all("div", {"class": "nova-o-stack__item"}):
+            research_link_section = research_item.find("a", {"class": "nova-e-link"})
+            research_link = research_link_section["href"]
+            research_title = research_link_section.text
+            try:
+                abstract = research_item.find("div", {"class": "nova-v-publication-item__description"}).text
+            except:
+                abstract = ""
+            research.append({"author": author, "research_link": research_link, "research_title": research_title, "abstract": abstract})
+    return research
